a1f65efbe2f5559e4a47f9443e24885c493a9015
Vitalii <Repz2010@gmail.com>
2018-06-21 00:49:39 +0300

    Update XO.rb
    style guide ruby

--- XO.rb
+++ XO.rb
@@ -1,6 +1,6 @@
 xo = ["камень",
-		  "ножницы",
-		  "бумага"
+      "ножницы",
+      "бумага"
 ] # Массив с данными
 
 comp_number = rand(3) # Присваиваем переменную случайному пислу от 0 - 2
@@ -12,38 +12,35 @@ puts "Компьютер хочет поиграть в игру Камень - 
 puts "Для игры нужно ввести один из вариантов в цифровом виде."
 puts "Введите вариант: 0 - камень, 1 - ножницы, 2 - бумага" # Ввод данных пользователя для начала игры
 
-
 user_number = gets.to_i # Переменная для ввода числа пользователя""
 enter_all = print "Вы выбрали #{xo[user_number]}, а Компьютер выбрал #{xo[comp_number]}"
 
-
-if ( user_number < 0 || user_number > 2 ) #Если игрок ввел число вне пределела 0 - 2, игра заканчивается
-		puts "Компьютер не хочет играть, если ты не соблюдаешь правила игры!"
-		puts "Нужно ввести в диапазоне от 0 - 2"
-	else
-	if ( comp_number == user_number ) # Условие для ничьей
-			puts enter_all
-			puts "Ничья"
-		else # Условие для выигрыша или проигрыша
-			if ( comp_number == 0 && user_number == 1 )
-					puts enter_all
-					puts "Комп победил, так как Вы выбрали камень поломал ножницы!"
-			elsif ( comp_number == 0 && user_number == 2 )
-					puts enter_all
-					puts "Ты победил, бумага закатала камень!"
-			elsif ( comp_number == 1 && user_number == 2  )
-					puts enter_all
-					puts "Комп победил, ножницы разрезали бумагу!"
-			elsif ( comp_number == 1 && user_number == 0  )
-					puts enter_all
-					puts "Ты победил, камень поломал ножницы!"
-			elsif ( comp_number == 2 && user_number == 0 )
-					puts enter_all
-					puts "Комп победил, бумага закатала камень!"
-			elsif ( comp_number == 2 && user_number == 1 )
-					puts enter_all
-					puts "Ты победил, ножницы разрезали бумагу!"
-			end
-	end
+if user_number < 0 || user_number > 2 #Если игрок ввел число вне пределела 0 - 2, игра заканчивается
+  puts "Компьютер не хочет играть, если ты не соблюдаешь правила игры!"
+  puts "Нужно ввести в диапазоне от 0 - 2"
+else
+  if comp_number == user_number # Условие для ничьей
+    puts enter_all
+    puts "Ничья"
+  else # Условие для выигрыша или проигрыша
+    if comp_number == 0 && user_number == 1
+      puts enter_all
+      puts "Комп победил, так как Вы выбрали камень поломал ножницы!"
+    elsif comp_number == 0 && user_number == 2
+      puts enter_all
+      puts "Ты победил, бумага закатала камень!"
+    elsif comp_number == 1 && user_number == 2
+      puts enter_all
+      puts "Комп победил, ножницы разрезали бумагу!"
+    elsif comp_number == 1 && user_number == 0
+      puts enter_all
+      puts "Ты победил, камень поломал ножницы!"
+    elsif comp_number == 2 && user_number == 0
+      puts enter_all
+      puts "Комп победил, бумага закатала камень!"
+    elsif comp_number == 2 && user_number == 1
+      puts enter_all
+      puts "Ты победил, ножницы разрезали бумагу!"
+    end
+  end
 end
-
